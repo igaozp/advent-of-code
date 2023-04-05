6d227e1b79a1f43173324fbb2aaa6364f6426a06
igaozp <igaozp@hotmail.com>
2023-04-05 21:26:06 +0800

    feat: Day 1 - Java

---
+++ day1/Main.java
@@ -0,0 +1,32 @@
+package day1;
+
+import java.io.*;
+import java.util.Objects;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        var path = Objects.requireNonNull(Main.class.getClassLoader().getResource("")).getPath();
+        var file = new File(path + "/day1/input.txt");
+
+        var content = "";
+        try (InputStream inputStream = new FileInputStream(file)) {
+            byte[] bytes = inputStream.readAllBytes();
+            content = new String(bytes);
+        }
+
+        String[] split = content.split("\r\n");
+
+        var max = 0;
+        var sum = 0;
+        for (String line : split) {
+            if ("".equals(line)) {
+                max = Math.max(max, sum);
+                sum = 0;
+            } else {
+                sum += Integer.parseInt(line);
+            }
+        }
+
+        System.out.println(max);
+    }
+}
