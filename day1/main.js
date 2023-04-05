fdd1740e34692ece3fc887f6e775c855b874cf58
igaozp <igaozp@hotmail.com>
2023-04-05 21:54:22 +0800

    feat: Day 1 - JavaScript

---
+++ day1/main.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+
+fs.readFile(__dirname + '/input.txt', 'utf-8', (err, data) => {
+    if (err) {
+        console.error(err);
+        return
+    }
+
+    let strings = data.split("\r\n");
+    let max = 0;
+    let sum = 0;
+    for (let line of strings) {
+        if ("" !== line) {
+            max = Math.max(max, Number.parseInt(line))
+            sum = 0
+        } else {
+            sum += Number.parseInt(line)
+        }
+    }
+    console.log(max)
+})
