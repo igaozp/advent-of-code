300e3bf010cb099644bc7f1aa70f3c64e405f542
igaozp <igaozp@hotmail.com>
2023-04-19 22:07:31 +0800

    feat: Day 1 - Rust 代码优化

--- day1/rust/src/main.rs
+++ day1/rust/src/main.rs
@@ -1,20 +1,22 @@
-use std::fs;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 
 fn main() {
-    let contents = fs::read_to_string("../input.txt")
-        .expect("Should have been able to read the file");
-
-    let split: Vec<&str> = contents.split("\r\n").collect();
+    let file = File::open("../input.txt").unwrap_or_else(|_| panic!("Could not read file"));
+    let reader = BufReader::new(file);
 
     let mut max = 0;
     let mut sum = 0;
-    for line in split {
-        if line == "" {
-            max = std::cmp::max(max, sum);
-            sum = 0
-        } else {
-            sum += line.parse::<i32>().unwrap()
+    for line in reader.lines() {
+        let line = line.unwrap_or_else(|_| panic!("Could not parse line"));
+        match line.parse::<i32>() {
+            Ok(num) => sum += num,
+            Err(_) if line.is_empty() => {
+                max = max.max(sum);
+                sum = 0;
+            }
+            _ => panic!("Could not parse line"),
         }
     }
-    println!("{max}")
+    println!("{}", max)
 }
